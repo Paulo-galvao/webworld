731048f96c0f2f2d652e745acb7e054bf29def74
Paulo-galvao <85948344+Paulo-galvao@users.noreply.github.com>
2021-09-19 12:36:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.slide').slick(
+    {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    }
+);
